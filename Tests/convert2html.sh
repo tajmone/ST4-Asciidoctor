6153711d2698f1b24c81509de7ff8e6ad4b9e00f
tajmone <tajmone@libero.it>
2019-09-24 16:18:52 +0200

    Tests Script: Delete old HTML Files

--- Tests/convert2html.sh
+++ Tests/convert2html.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Delete old HTML files...
+find . -name "*.html" -type f -delete
+
+# Convert all from scratch...
 for i in *.asciidoc **/*.asciidoc ; do
   asciidoctor \
     -a toc=left \
