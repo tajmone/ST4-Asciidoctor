4f25b2958a1d1d966f357cd5fba3ed1c281285f1
Craig Jones <craig@k6nnl.com>
2021-07-24 00:58:16 -0700

    Added a new command: "AsciiDoc: Fix Up Converted Prose". See FIXUP_CONVERTED.adoc for details.

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 from sublime_plugin import TextCommand
+import sublime
 import re
 
 
@@ -69,3 +70,109 @@ class AsciidocRunCommandsCommand(TextCommand):
                 self.view.run_command(command)
             else:
                 self.view.run_command(command[0], *command[1:])
+
+
+class AsciidocProseFixupCommand(TextCommand):
+    """ 
+    Cleans up a manuscript that has been converted to AsciiDoc, e.g. by PanDoc...
+
+    pandoc --from=docx --to=asciidoc --wrap=none --atx-headers --extract-media=extracted-media $FILENAME.docx > $FILENAME..adoc
+
+    """
+    def run(self, edit):
+        self._edit = edit
+        self._process_text()
+
+    def _get_file_content(self):
+        return self.view.substr(sublime.Region(0, self.view.size()))
+
+    def _update_file(self, doc):
+        self.view.replace(self._edit, sublime.Region(0, self.view.size()), doc)
+
+    def _process_text(self):
+        txt = self._get_file_content()
+
+        # Remove any leading spaces
+        txt = re.sub(r"^[ \t]+", "", txt)
+
+        # Remove any trailing spaces
+        txt = re.sub(r"[ \t]+$", "", txt)
+
+        # Collapse multiple spaces
+        txt = re.sub(r"\t", " ", txt)
+        txt = re.sub(r"  +", " ", txt)
+
+        # Delete any IDs automatically inserted by PanDoc
+        txt = re.sub(r"\[\[_.*]]", "", txt)
+
+        # Shorten table delimiters
+        txt = re.sub(r"\|==+", "|===", txt)
+
+        # convert m-dashes to AsciiDoc syntax
+        txt = re.sub(r"—", "--", txt)
+
+        # Ensure exactly one space before and after ellipses and m-dashes that are mid-sentence (between words)
+        txt = re.sub(r"(\w) *\.\.\.+ *(\w)", "\\1 ... \\2", txt)
+        txt = re.sub(r"(\w) *-- *(\w)", "\\1 -- \\2", txt)
+
+        # Make sure all m-dashes are represented by exactly 2 dashes
+        txt = re.sub(r" +-+ +", " -- ", txt)
+
+        #Replace figure captions with id and title
+        txt = re.sub(r"^Figure (\d+)\s?(.*)", "[[fig-\\1]]\n.\\2\n", txt)
+
+        # Replace references to figures with asciidoc xref
+        txt = re.sub(r"Figure (\d+)", "<<fig-\\1>>", txt)
+
+        # Smarten dumb quotes (make them typographic)
+        txt = re.sub(r"(^|[^a-z])'(em|do|tis|twas|til)\b", "\\1{rsquo}\\2", txt, flags=re.IGNORECASE)
+        txt = re.sub(r"^\"", '"`', txt)
+        txt = re.sub(r'^_"', '"`_', txt)
+        txt = re.sub(r' "', ' "`', txt)
+        txt = re.sub(r'"_(\W*)$', '_`"\\1', txt)
+        txt = re.sub(r'"(\W*)$', '`"\\1', txt)
+        txt = re.sub(r'"([- .,!?])', '`"\\1', txt)
+        txt = re.sub(r'"_([- .,!?])', '_`"\\1', txt)
+        txt = re.sub(r"^'", "'`", txt)
+        txt = re.sub(r" '", " '`", txt)
+        txt = re.sub(r"'\n", "`'", txt)
+        txt = re.sub(r"'([- .,!?])", "`'\\1", txt)
+
+        txt = re.sub(r'``"', '`"', txt)
+        txt = re.sub(r"``'", "`'", txt)
+
+        # Undo smart quotes, making sure to distinguish a possessive apostrophe from a closing quote
+        txt = re.sub(r'“', '"`', txt)
+        txt = re.sub(r'”', '`"', txt)
+        txt = re.sub(r"‘", "'`", txt)
+        txt = re.sub(r"(\w)’(\w)", "\\1'\\2", txt)
+        txt = re.sub(r"’", "`'", txt)
+
+        # Ensure bullet point syntax (exactly one space following)
+        txt = re.sub(r"^(-+|\*+|\.+) *", "\\1 ", txt)
+
+        # Fix transposed end-of-quotation punctuation -- that is, the punctuation goes inside the quotes
+        txt = re.sub(r'`"(,|\.|\?|\!)', "\\1`\"", txt)
+        txt = re.sub(r"`'(,|\.|\?|\!)", "\\1`'", txt)
+
+        # Fix misplaced commas near parentheticals 
+        txt = re.sub(r",( *\([^)]*?\))", "\\1,", txt)
+
+        # One sentence per line -- allowing for closing quotes and closing italics
+        txt = re.sub(r'(\.|\?|\!)(`?["\']?)(_?) +([^a-z])', "\\1\\2\\3\n\\4", txt)
+
+        # Fix false line breaks after abbreviations, single initials and ellipses 
+        txt = re.sub(r'(Mrs?|Ms|Drs?|Prof|\baka|\bp|\badj|\bn|\bv|a\.k\.a|e\.g|\bvs|\best|i\.e|P\.O| [A-Z]| \.\.)\. *\n', '\\1. ', txt, flags=re.MULTILINE)
+
+        # HTML Entities to Attributes
+        txt = re.sub(r"&apos;", "{rsquo}", txt)
+        txt = re.sub(r"&(\w*);", "{\\1}", txt)
+
+        # Scene breaks
+        txt = re.sub(r"^ *(#|\*)+ *$", "'''", txt, flags=re.MULTILINE)
+
+        # AsciiDoctor can't handle m-dashes at the end of a quotation.
+        txt = re.sub(r' *-- *`"', '{mdash}`"', txt)
+
+        self._update_file(txt)
+        
